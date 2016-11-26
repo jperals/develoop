15640ee09ae28eb34faac62b9b68244b31207e03
Joan Perals <joan.perals@ixds.com>
2016-11-27 00:10:32 +0100

    Return snippet by title

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var Promise = require('promise');
 var request = require('request');
 var settings = require('./settings');
+
+require('promise/lib/rejection-tracking').enable(
+    {allRejections: true}
+);
 
 if(settings.testing) {
     // Accept self-signed certificate.
@@ -13,28 +18,83 @@ var app = express();
 
 app.use(express.static('client'));
 
-app.get('/component/:id', function(req, res) {
-    console.log('Try to get component', req.params.id);
+app.get('/component/:name', function(req, res) {
+    console.log('Trying to get component named', req.params.name);
 
-    var options = {
-        url: settings.gitlabHost + '/api/v3/projects/1/snippets/' + req.params.id + '/raw',
-        headers: {
-            'PRIVATE-TOKEN': settings.gitlabToken
-        }
-    };
-
-    function callback(error, response, body) {
-        console.log(error, response, body);
-        if (!error && response.statusCode == 200) {
-            res.send(body);
-        }
-    }
-
-    request(options, callback);
+    getAllSnippets()
+        .then(function(snippets) {
+            return getIdByTitle(snippets, req.params.name)
+        })
+        .then(getSnippetById)
+        .then(function(snippet) {
+            res.send(snippet);
+        })
+        .catch(function(error) {
+            console.error('Error!', error);
+            res.sendStatus(500);
+        });
 
 });
 
 app.listen(3000, function () {
     console.log('App listening on port 3000!');
-    console.log('Settings:', settings);
 });
+
+function getAllSnippets () {
+    var promise = new Promise(function (resolve, reject) {
+        var options = {
+            url: settings.gitlabHost + '/api/v3/projects/1/snippets/',
+            headers: {
+                'PRIVATE-TOKEN': settings.gitlabToken
+            }
+        };
+        function callback(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                resolve(JSON.parse(body));
+            }
+            else {
+                reject(error);
+            }
+        }
+        request(options, callback);
+    });
+    return promise;
+}
+
+function getIdByTitle (snippets, title) {
+    var promise = new Promise(function (resolve, reject) {
+        var snippet = snippets.find(function(s) {
+            return s.title === title;
+        });
+        console.log('snippet:', snippet);
+        if (typeof snippet !== 'undefined') {
+            resolve(snippet.id);
+        }
+        else {
+            reject('No snippet found with the given name.');
+        }
+    });
+    return promise;
+}
+
+function getSnippetById (id) {
+    var promise = new Promise(function (resolve, reject) {
+        var options = {
+            url: settings.gitlabHost + '/api/v3/projects/1/snippets/' + id + '/raw',
+            headers: {
+                'PRIVATE-TOKEN': settings.gitlabToken
+            }
+        };
+        function callback(error, response, body) {
+            // console.log(error, response, body);
+            if (!error && response.statusCode == 200) {
+                resolve(body);
+            }
+            else {
+                reject(error);
+            }
+        }
+        request(options, callback);
+    });
+    return promise;
+}
