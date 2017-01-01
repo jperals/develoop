0c8e9ff285098c0a613ff04f7cca897763cba6ee
Joan Perals <joan.perals@ixds.com>
2017-01-02 00:36:05 +0100

    Add settings.js

---
+++ server/settings.js
@@ -0,0 +1,13 @@
+var gitlabHost= 'https://127.0.0.1:8443';
+var gitlabToken = 'XXXX';
+var gitlabNamespace = 'designer';
+var gitlabProjectName = 'components';
+var testing = true;
+
+module.exports = {
+    gitlabHost: gitlabHost,
+    gitlabToken: gitlabToken,
+    gitlabNameSpace: gitlabNamespace,
+    gitlabProjectName: gitlabProjectName,
+    testing: testing
+};
